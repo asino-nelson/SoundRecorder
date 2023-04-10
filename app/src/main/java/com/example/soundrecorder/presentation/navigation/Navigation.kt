5ca7b0a5e2523e5833e788fbb280c29baf571e8e
asino-nelson <asinonelson276@gmail.com>
2023-04-10 14:23:59 +0300

    Navigation update commit

--- app/src/main/java/com/example/soundrecorder/presentation/navigation/Navigation.kt
+++ app/src/main/java/com/example/soundrecorder/presentation/navigation/Navigation.kt
@@ -5,12 +5,17 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.soundrecorder.presentation.play.PlayScreen
+import com.example.soundrecorder.data.Record
 import com.example.soundrecorder.presentation.recorded_list.MyRecordings
+import com.example.soundrecorder.presentation.recorded_list.RecordsViewModel
 import com.example.soundrecorder.presentation.recording_screen.RecordScreen
 
 @Composable
-fun Navigation(context: Context){
+fun Navigation(
+    context: Context,
+    recordsViewModel: RecordsViewModel,
+    record: Record
+){
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = Screens.RecordingScreen.route ){
         composable(route = Screens.RecordingScreen.route){
@@ -18,7 +23,11 @@ fun Navigation(context: Context){
         }
 
         composable(route = Screens.RecordsScreen.route){
-            MyRecordings(navController = navController)
+            MyRecordings(
+                record = record,
+                navController = navController,
+                recordsViewModel = recordsViewModel
+            )
         }
     }
 
